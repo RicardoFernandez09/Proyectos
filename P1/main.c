64486fe84e3b523ba81aa510b8554fc6f7a1bc55
Rick001 <rick0092@hots.com>
2024-09-13 13:51:57 -0400

    Practica de productos

--- P1/main.c
+++ P1/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+#include <stdio.h>
+
+/* incremento de precio.
+El programa, al recibir como dato el precio de un producto, incrementa al
+mismo 11% si es menor a 1500$ y 8% en caso contrario (mayor o igual).
+
+PRE Y NPR: variables de tipo real. */
+
+void main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+float PRE, NPR;
+printf("Ingrese el precio del producto: ");
+scanf("%f", &PRE);
+if (PRE < 1500)
+        NPR = PRE * 1.11;
+else
+    NPR = PRE * 1.08;
+printf("\nNuevo precio del producto: %8.2f", NPR);
 }
